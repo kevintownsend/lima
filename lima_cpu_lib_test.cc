4c2dc342d47fc41b29c407ac361cc9cba32f164c
Kevin Townsend <kevintownsend4219@gmail.com>
2023-03-28 17:59:22 -0700

    .

--- lima_cpu_lib_test.cc
+++ lima_cpu_lib_test.cc
@@ -1,11 +1,23 @@
 #include "gtest/gtest.h"
 #include "lima_cpu_lib.h"
 
+#include <vector>
+#include <cinttypes>
+
 // TODO: include test include.
 namespace {
 
 TEST(HELLO, WORLD) {
   EXPECT_STRNE("hello", "world");
+}
+
+TEST(CONVERT, SIMPLE) {
+  EXPECT_EQ(convert(42), std::vector<uint64_t>({42ULL}));
+}
+
+TEST(CONVERT, TWO_VALUE_VECTOR) {
+  EXPECT_EQ(convert(0xdeadbeefULL, 8),
+      std::vector<uint64_t>({0xefULL, 0xbeULL, 0xadULL, 0xdeULL}));
 }
 
 }
