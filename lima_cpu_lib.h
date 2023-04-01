17705ee0e1797455ac71aa98878bedef1a8d9a76
Kevin Townsend <kevintownsend4219@gmail.com>
2023-03-31 19:25:34 -0700

    .

--- lima_cpu_lib.h
+++ lima_cpu_lib.h
@@ -7,5 +7,13 @@
 
 std::vector<uint64_t> convert(boost::multiprecision::cpp_int value, int bitwidth = 64);
 
+// TODO: multiply
+
+void multiply(std::vector<uint64_t> &operand, std::vector<uint64_t> &result);
+
+// TODO: modulo
+
+// TODO: loop
+
 #endif // LIMA_CPU_LIB_H_
 
