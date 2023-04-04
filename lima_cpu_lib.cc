33eab81abd0890174043fe966b5ebe55cae57bd8
Kevin Townsend <kevintownsend4219@gmail.com>
2023-04-04 15:41:37 -0700

    .

--- lima_cpu_lib.cc
+++ lima_cpu_lib.cc
@@ -6,6 +6,7 @@
 #include <boost/multiprecision/cpp_int.hpp>
 
 const extern uint64_t BASE_BITS = 26;
+const uint64_t BASE_MASK = -1ULL >> (64 - BASE_BITS);
 
 std::vector<uint64_t> convert(boost::multiprecision::cpp_int value, int bitwidth) {
   uint64_t mask = -1ULL >> (64-bitwidth);
@@ -32,5 +33,19 @@ boost::multiprecision::cpp_int to_cpp_int(std::vector<uint64_t> value, int bitwi
 void multiply(std::vector<uint64_t> &operand, std::vector<uint64_t> &result){
   std::fill(result.begin(), result.end(), 0);
   uint64_t operand_size = operand.size();
+  for(int i = 0; i < operand_size; i++) {
+    result[2*i] += operand[i] * operand[i];
+    for(int j = i + 1; j < operand_size; j++) {
+      result[i+j] += 2 * operand[i] * operand[j];
+    }
+  }
+
+  uint64_t carry = 0;
+  for(int i = 0; i < result.size(); i++) {
+    result[i] += carry;
+    carry = result[i] >> BASE_BITS;
+    result[i] &= BASE_MASK;
+  }
+
   // TODO: complete.
 }
--- lima_cpu_lib_test.cc
+++ lima_cpu_lib_test.cc
@@ -4,12 +4,7 @@
 #include <vector>
 #include <cinttypes>
 
-// TODO: include test include.
 namespace {
-
-TEST(HELLO, WORLD) {
-  EXPECT_STRNE("hello", "world");
-}
 
 TEST(CONVERT, SIMPLE) {
   EXPECT_EQ(convert(42), std::vector<uint64_t>({42ULL}));
@@ -18,6 +13,13 @@ TEST(CONVERT, SIMPLE) {
 TEST(CONVERT, TWO_VALUE_VECTOR) {
   EXPECT_EQ(convert(0xdeadbeefULL, 8),
       std::vector<uint64_t>({0xefULL, 0xbeULL, 0xadULL, 0xdeULL}));
+}
+
+TEST(MULTIPLY, SINGLE_VALUE) {
+  std::vector<uint64_t> result({0});
+  std::vector<uint64_t> operand({2});
+  multiply(operand, result);
+  EXPECT_EQ(result, std::vector<uint64_t>({4}));
 }
 
 }
